9e04af97acedb5ad9145c7c865e308feddda4a96
tiancai110a <389006500@qq.com>
2019-06-16 18:37:07 +0800

    热更新

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
+	"log"
+	"syscall"
+
+	"github.com/fvbock/endless"
 
 	"github.com/golang/glog"
 
@@ -17,29 +18,33 @@ func init() {
 	flag.Set("alsologtostderr", setting.LOG_STDERR) // 日志写入文件的同时，输出到stderr
 	flag.Set("log_dir", setting.LOG_PATH)           // 日志文件保存目录
 	flag.Set("v", setting.LOG_LEVEL)                // 配置V输出的等级。
+	glog.Info("================================================", setting.LOG_LEVEL)
 	flag.Parse()
 
 }
 
 func main() {
-	router := routers.InitRouter()
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt, os.Kill)
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, os.Kill)
+	// go func(c chan os.Signal) {
+	// 	<-c
+	// 	glog.Flush()
+	// 	os.Exit(0)
+	// }(c)
 
-	go func(c chan os.Signal) {
-		<-c
-		glog.Flush()
-		os.Exit(0)
-	}(c)
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        router,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
 }
